5617a0ae6fc4f0e70a0f40cb3cd395c71c635b24
Larry Lau <lsl1840@gmail.com>
2022-10-31 17:40:52 -0500

    10/31 EE section

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Generalinfo from './App';
+import Educationalexperience from './components/Educationalexperience';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Generalinfo/>
+    <Educationalexperience/>
   </React.StrictMode>
 );
 
